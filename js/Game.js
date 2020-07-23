32956ec6ae7766e3438f8b5fe4acd6fbcaa9a757
hritikaswani <jharnataswani@gmail.com>
2020-07-23 17:50:49 +0530

    car

--- js/Game.js
+++ js/Game.js
@@ -55,7 +55,7 @@ class Game {
       var index = 0;
 
       //x and y position of the cars
-      var x = 175 ;
+      var x = 290 ;
       var y;
 
       for(var plr in allPlayers){
@@ -63,7 +63,7 @@ class Game {
         index = index + 1 ;
 
         //position the cars a little away from each other in x direction
-        x = x + 200;
+        x = x + 280;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
         cars[index-1].x = x;
@@ -71,6 +71,9 @@ class Game {
 
         if (index === player.index){
           cars[index - 1].shapeColor = "red";
+          stroke (10);
+        fill ("red");
+        ellipse(x,y,60,60);
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y;
         }
@@ -84,9 +87,10 @@ class Game {
     if(keyIsDown(UP_ARROW) && player.index !== null){
       player.distance +=10
       player.update();
+      console.log(player.distance);
     }
 
-    if(player.distance > 3860){
+    if(player.distance > 5340){
       gameState = 2;
     }
    
